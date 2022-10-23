654704c6ace9ad661e0cc257fc74a982559c5a58
chobe111 <chobe1@naver.com>
2022-10-23 19:53:46 +0900

    Update test code

--- test.py
+++ test.py
@@ -3,14 +3,15 @@ from services.BeatService import BeatService
 import os
 import dotenv
 dotenv.load_dotenv()
-test_input_wav_path = os.environ.get("")
+test_input_wav_path = os.environ.get("INPUT_WAV_PATH")
 class BeatGenerateTest(unittest.TestCase):
-    def test_beat_generate():
+    def test_beat_generate(self):
+        
+        print(test_input_wav_path)
+        
         service = BeatService(test_input_wav_path)
         bpm, beats = service.start()
-        
-        assert len(bpm) != 0
-        
+        assert len(beats) != 0
         print("bpm = ", bpm)
         print("beats = ", beats)
 
